80da176ce9e95ee574b72cba76721c5c981d9831
zbigniewkalinowski <34089686+zbigniewkalinowski@users.noreply.github.com>
2024-06-11 16:40:15 +0200

    unpack

---
+++ SzkolenieNodeJS-Zadanie1-rozwiazanie/SzkolenieNodeJS-Zadanie1-rozwiazanie/Czesc1/Zadanie1/Zadanie1.js
@@ -0,0 +1,14 @@
+import fs from "fs";
+
+export const readData = () => {
+  try {
+    const data = fs.readFileSync("../dane.json");
+    const dataParsed = JSON.parse(data);
+    console.log(dataParsed);
+    return dataParsed;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+readData();
